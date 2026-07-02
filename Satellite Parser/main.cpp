e4670b3a748675abe200b29b9c52d7d5e58ffcbf
Mamadou Diallo <mdiallo7@students.towson.edu>
2026-07-01 21:48:23 -0400

    Initial commit

---
+++ Satellite Parser/main.cpp
@@ -0,0 +1,12 @@
+﻿// Satellite Parser.cpp : Defines the entry point for the application.
+//
+
+#include "Satellite Parser.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
